948e3a7ba2175c03a1dfe409dad52fcf91188855
kay- <kay.yamagishi.work@gmail.com>
2014-12-15 20:58:43 -0800

    modify mobile view in 480px

--- htdocs/js/javascript.js
+++ htdocs/js/javascript.js
@@ -2,14 +2,19 @@ window.onload = function () {
   setGreetHeight();
 };
 
-window.onscroll = function() {
-  visibl.set("getLine1", "setLine", 0.9, true);
-  visibl.set("getLine2", "setLine", 0.9, true);
-  visibl.set("getLine3", "setLine", 0.9, false);
-  visibl.set("getLine4", "setLine", 0.9, false);
-  visibl.set("getAnimationFloat", "setAnimationFloat", 0.7, true);
-  visibl.set("getAnimationFloat2", "setAnimationFloat2", 0.8, true);
-};
+var w = window.innerWidth;
+
+if (w > 480) {
+  window.onscroll = function() {
+    visibl.set("getLine1", "setLine", 0.9, true);
+    visibl.set("getLine2", "setLine", 0.9, true);
+    visibl.set("getLine3", "setLine", 0.9, false);
+    visibl.set("getLine4", "setLine", 0.9, false);
+    visibl.set("getAnimationFloat", "setAnimationFloat", 0.7, true);
+    visibl.set("getAnimationFloat2", "setAnimationFloat2", 0.8, true);
+  };
+}
+
 
 window.onresize = function() {
 
